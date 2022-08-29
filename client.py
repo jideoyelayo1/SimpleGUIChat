a36da829e823eb7709814f3d5966442d498ea9aa
JideO <JideOyelayo1@Gmail.com>
2022-08-29 14:20:54 +0100

    stop fcn

--- client.py
+++ client.py
@@ -7,27 +7,61 @@ from tkinter import simpledialog
 HOST = '192.168.5.66'  # ipconfig
 PORT = 9090
 
+
 class Client:
-    def __init__(self,host,port):
-        self.sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-        self.sock.connect((host,port))
+    def __init__(self, host, port):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect((host, port))
 
         msg = tkinter.Tk()
         msg.withdraw()
 
-        self.nickname = simpledialog.askstring("Nickname","Please choose a nickname",parent=msg)
+        self.nickname = simpledialog.askstring("Nickname", "Please choose a nickname", parent=msg)
         self.gui_done = False
         self.running = True
 
         gui_thread = threading.Thread(target=self.gui_loop)
-        recieve_thread = threading.Thread(target=self.recieve)
-
+        recieve_thread = threading.Thread(target=self.receive)
 
         gui_thread.start()
         recieve_thread.start()
 
-        def gui_loop(self):
-            pass
+    def gui_loop(self):
+        self.win = tkinter.Tk()
+        self.win.configure(bg="lightgray")
 
-        def recieve(self):
-            pass
+        self.chat_label = tkinter.Label(self.win, text="Chat:", bg="lightgray")
+        self.chat_label.config(font=("Arial", 12))
+        self.chat_label.pack(padx=20, pady=5)
+
+        self.text_area = tkinter.scrolledtext.ScrolledText
+        self.text_area.pack(padx=20, pady=5)
+        self.text_area.config(state='disabled')
+
+        self.msg_label = tkinter.Label(self.win, text="Message:", bg="lightgray")
+        self.msg_label.config(font=("Arial", 12))
+        self.msg_label.pack(padx=20, pady=5)
+
+        self.input_area = tkinter.Text(self.win, height=5, bg="blue")
+        self.input_area.pack(padx=20, pady=5)
+
+        self.send_button = tkinter.Button(self.win, text="Send", command=self.write)
+        self.send_button.config(font=("Arial", 12))
+
+        self.gui_done = True
+
+        self.win.protocol("WM_DELETE_WINDOW", self.stop)
+
+        self.win.mainloop()
+
+    def write(self):
+        pass
+
+    def stop(self):
+        self.running = False
+        self.win.destroy()
+        self.sock.close()
+        exit(0)
+
+    def receive(self):
+        pass
